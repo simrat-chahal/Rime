0490dfc85fc5b19c1620b28720e4b885ddaafd45
simratChahal <9892681-simratChahal@users.noreply.gitlab.com>
2023-02-19 21:12:06 +0530

    webpack config is updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,14 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
 module.exports = {
-  entry: 'index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
-  },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  plugins: [new HtmlWebpackPlugin()],
 };
